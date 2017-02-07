6f493f07cf2965fa7be81882c72b09013cb305ad
Abhishek Tiwari <erabhishektiwarics@gmail.com>
2017-02-07 13:50:25 +0530

    add

--- parallel.cpp
+++ parallel.cpp
@@ -14,7 +14,7 @@ using namespace std;
 //https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-096-algorithms-for-computational-biology-spring-2005/lecture-notes/lecture5_newest.pdf
 int solve(string &pat1,int &len1,string &pat2,int &len2)
 {
-
+vector<vector<int> >dp(len1+2,vector<int>(len2+2,0));
 for(int i=0;i<len1+1;i++)
 	dp[i][0]=0;
 for(int j=0;j<len2+1;j++)
@@ -34,54 +34,6 @@ for(int i=0;i<len1;i++)
 return dp[len1][len2];
 }
 
-/*
-int solve_str(string s1,int len1,string s2, int len2,int window,int threshold,int anchor_ind,int left,int max_len, vector<pair<int,int> > &anchor_score) //first int is score second int is index of text
-{
-
-if(anchor_ind<0||anchor_ind>=max_len)
-return 0;
-
-string pat_str=s1.substr(anchor_ind,window);
-vector<vector<int> > dp(window+2, vector<int>(window+2,0));
-
-
-for(int i=0;i<len2-window+1;i++)
-{
-int score=0;
-string text=s2.substr(i,window);
-if((score=solve(pat_str,window,text,window,dp))>=threshold)
-{
-//calculate left score
-string left_pat=s1.substr(0,anchor_ind);
-string right_pat=s1.substr(anchor_ind+window,len1-anchor_ind-window+1);
-string left_text=s2.substr(0,i);
-string right_text=s2.substr(i+window,len2-i-window+1);
-int lpl=left_pat.length();
-int rpl=right_pat.length();
-int ltl=left_text.length();
-int rtl=right_text.length();
-
-int left=solve_str(left_pat,lpl,left_text,ltl,window,threshold,anchor_ind-1,anchor_score);
-//calculate right score
-int right=solve_str(right_pat,rpl,right_text,rtl,window,threshold,anchor_ind+window+1,anchor_score);
-
-if(anchor_score[anchor_ind].first<= score+left+right){
-anchor_score[anchor_ind].first=score+left+right;
-anchor_score[anchor_ind].second=i;
-
-}
-
-}
-
-
-}
-
-
-
-
-
-}
-*/
 
 int main(){
 string s1,s2;//s1 is to be searched s2 is text
@@ -90,8 +42,8 @@ int len1=s1.length(),len2=s2.length();
 int window,threshold;
 cin>>window>>threshold;
 
-vector<pair<string,int> > anchor_score;
-
+vector<pair<int,int> > anchor_score;
+int max_score=0,align=-1;
 vector<string> anchor(len1-window+1,"");
 
 int max_align=0;
@@ -111,11 +63,24 @@ string text_mat=s1.substr(i,window);
 int score=solve(anchor[j],window,text_mat,window);
 if(score>=threshold)
 {
-achor_score(push_back(make_pair(anchor[j],i)));
+anchor_score(push_back(make_pair(j,i)));
 }
 }
 }
 
+for(int i=0;i<anchor_score.size();i++)
+{
+string pat_mat=anchor[anchor_score[i].first];
+int left=2*(anchor_score-0);
+int right=2*(len1-anchor_score-window+1);
+int total_len=left+right+window;
+int starting_ind=max(0,anchor_score[i].second-left);
+int ending_ind=min(anchor_score[i].second+window+right,len2-1);
+int char_len=ending_ind-starting_ind+1;
+string pat_text=s2.substr(starting_ind,char_len);
 
+
+
+}
 	return 0;
 }
